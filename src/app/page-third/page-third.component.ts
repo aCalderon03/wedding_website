a573212226f8aba8d9b7a9670e4bca777b99591f
AC <alejandroc744255@gmail.com>
2025-05-22 03:14:33 -0400

    routing adding to pagetwo and three, pages have buttons to test links on build, and styling was moved to styles

--- src/app/page-third/page-third.component.ts
+++ src/app/page-third/page-third.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-third',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page-third.component.html',
   styleUrl: './page-third.component.scss'
 })
--- src/app/page-two/page-two.component.ts
+++ src/app/page-two/page-two.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-two',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page-two.component.html',
   styleUrl: './page-two.component.scss'
 })
